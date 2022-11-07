06a2c7775802a2c136b0f2c561079fe9716d4c97
Samuel Lubelsky <slubelsky30@stuy.edu>
2022-11-07 09:53:47 -0500

    devTools db finished

--- app/db_tools.py
+++ app/db_tools.py
@@ -19,30 +19,72 @@ def get_table_list(tableName):
     res = c.execute(f"SELECT * from {tableName}")
     return res.fetchall()
 #add data row to sqlTable, put row in list format
+#if username already in db returns -1
 def add_account(username, password):
-    c.execute("INSERT INTO UserInfo (username, password) VALUES (?, ?);", (username, password))
+    if not(account_exists(username)):
+        c.execute("INSERT INTO UserInfo (username, password) VALUES (?, ?);", (username, password))
+    else:
+        return -1
+def account_exists(username):
+    accounts = get_table_list("UserInfo")
+    for account in accounts:
+        if account[0] == username:
+            return True
+    return False
+#return true if username and password are in db, false if one isn't
+def verify_account(username, password):
+    accounts = get_table_list("UserInfo")
+    for account in accounts:
+        if account[0] == username and account[1] == password:
+            return True
+    return False
+
+
+def story_exists(storyName):
+    stories = get_table_list("StoryInfo")
+    for story in stories:
+        if story[0] == storyName:
+            return True
+    return False
+
+#add new story to db
+#if a story with that name already exists 
 def add_story(storyName, newText, contributor):
-    c.execute("INSERT INTO StoryInfo VALUES (?, ?, ?, ?)", (storyName, newText, newText, contributor))
+    if not(story_exists):
+        c.execute("INSERT INTO StoryInfo VALUES (?, ?, ?, ?)", (storyName, newText, newText, contributor))
+    else:
+        return -1
+#return fullText, lastAdded, and contributors of a story
+#return -1 if story is not in db
 def get_story_info(storyName):
-    storyInfo = get_table_list()
-    targetStory = storyInfo[storyInfo.find(storyName)]
-    return targetStory[1:]
+    storyInfo = get_table_list("StoryInfo")
+    for row in storyInfo:
+        if row[0] == storyName:
+            return row[1:]
+    return -1
+#edit story
 def edit_story(storyName, newText, contributor):
-    storyInfo = get_story_info(storyName)
-    fullText = storyInfo[0] + newText
-    contributors = contributor + "," + storyInfo[2] 
-    c.execute(f'''
-    UPDATE storyInfo
-    SET fullStory = {fullText},
-    lastAdded = {newText},
-    Contributors = {contributors}
-    WHERE
-    storyName = {storyName}
-    ''')
+    if(story_exists(storyName)):
+        storyInfo = get_story_info(storyName)
+        fullText = storyInfo[0] + newText
+        contributors = contributor + "," + storyInfo[2] 
+        c.execute(f'''
+        UPDATE storyInfo
+        SET fullStory = "{fullText}",
+        lastAdded = "{newText}",
+        Contributors = "{contributors}"
+        WHERE
+        storyName = "{storyName}"
+        ''')
+    else:
+        return -1
 
 #add_story("beeInfo", "bees are cool", "SamLublsky")
-edit_story("beeInfo")
-print(get_table_list("storyInfo"))
+# print(edit_story("beeInfo", ".  I hate bees :(", "Ayman"))
+# print(get_table_list("storyInfo"))
+#print(verify_account("hello", "world"))
+#print(add_account("my", "name"))
+#print(get_table_list("UserInfo"))
 
 db.commit()
 db.close()
